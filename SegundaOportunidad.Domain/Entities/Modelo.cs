2e87e795bf8845a84289dd2823ebc3a76378cc43
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-24 22:31:31 -0400

    Agrega la clase Modelo

---
+++ SegundaOportunidad.Domain/Entities/Modelo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundaOportunidad.Domain.Entities
+{
+    public class Modelo
+    {
+        
+       public int Modelo_ID { get; set; }
+        public string Nombre { get; set; }
+
+        public Modelo()
+        {
+
+        }
+    }
+}
