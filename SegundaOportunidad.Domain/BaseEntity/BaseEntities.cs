38fa44d62154745720f25db4b7caa7ce30c9bff0
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-25 01:37:12 -0400

    Agrega archivos eliminados

---
+++ SegundaOportunidad.Domain/BaseEntity/BaseEntities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundaOportunidad.Domain.BaseEntity
+{
+    public class BaseEntities
+    {
+        public int UserId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int? UserMod { get; set; }
+        public DateTime? MoodifyDate { get; set; }
+
+        public BaseEntities()
+        {
+            this.CreationDate = DateTime.Now;
+
+        }
+
+    }
+}
---
+++ SegundaOportunidad.Repository/BaseRepository/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using SegundaOportunidad.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SegundaOportunidad.Domain;
+using SegundaOportunidad.Domain.BaseEntity;
+using System.Threading.Tasks;
+
+namespace SegundaOportunidad.Repository.BaseRepository
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntities
+    {
+        public Task Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
