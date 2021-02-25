62213bb48c0027b9b4867fca1f2e956018d340b2
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-25 00:14:21 -0400

    Agrega la clase ConceptoVenta al domain

---
+++ SegundaOportunidad.Domain/Entities/ConceptoVenta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundaOportunidad.Domain.Entities
+{
+    public class ConceptoVenta
+    {
+
+        public int Concepto_Venta_ID { get; set; }
+        public int Venta_ID { get; set; }
+        public int Producto_ID { get; set; }
+        public int Cantidad { get; set; }
+        public int Precio_Unitario { get; set; }
+        public int Importe_Producto { get; set; }
+
+        public ICollection<Venta> Ventas { get; set; }
+        public ICollection<Producto> Productos { get; set; }
+
+        public ConceptoVenta()
+        {
+            Ventas = new HashSet<Venta>();
+            Productos = new HashSet<Producto>();
+
+        }
+    }
+}
