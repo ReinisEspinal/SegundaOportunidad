301aa57c763c5a0ef92c993e68b95d4575df28c2
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-25 00:48:22 -0400

    Agrega interfaz IBaseRepository con 2 metodos

---
+++ SegundaOportunidad.Repository/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundaOportunidad.Repository.Interfaces
+{
+    interface IBaseRepository<TEntity> 
+    {
+        Task Add(TEntity entity);
+        Task <IEnumerable<TEntity>> GetAll();
+    }
+}
