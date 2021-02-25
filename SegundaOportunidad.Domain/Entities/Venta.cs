d35f87d94e8dac85976efc7c1187554646db5c65
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-24 22:09:41 -0400

    Agrega la clase Venta

---
+++ SegundaOportunidad.Domain/Entities/Venta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundaOportunidad.Domain.Entities
+{
+    public class Venta
+    {
+        public int Venta_ID { get; set; }
+        public int Persona_ID { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Venta_Total { get; set; }
+
+        public virtual ICollection<Persona> Peronas { get; set; }
+
+        public Venta()
+        {
+
+        }
+
+    }
+}
