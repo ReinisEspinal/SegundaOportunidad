b9bd8225bee8a4da6a400c4d74c814fc03a7d3f2
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-24 22:01:23 -0400

    Agrega la clase Persona

---
+++ SegundaOportunidad.Domain/Entities/Persona.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace SegundaOportunidad.Domain.Entities
+{
+    public class Persona
+    {
+        
+        public int Persona_ID { get; set; }
+        public string Nombre {get;set;}
+        public int Apellido { get; set;}
+        public int Tipo_Documento { get; set; }
+
+        public Persona()
+        {
+
+        }
+
+    }
+
+}
