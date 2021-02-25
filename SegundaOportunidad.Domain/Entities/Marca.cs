1b746fb6c6bb78b55a96c78f751915b7642f9447
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-24 22:42:50 -0400

    Agrega la clase Marca y cambia algunas propiedades en la clase Modelo

---
+++ SegundaOportunidad.Domain/Entities/Marca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundaOportunidad.Domain.Entities
+{
+    public class Marca
+    {
+        public int Marca_ID { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public Marca()
+        {
+
+        }
+    }
+}
--- SegundaOportunidad.Domain/Entities/Modelo.cs
+++ SegundaOportunidad.Domain/Entities/Modelo.cs
@@ -6,13 +6,16 @@ namespace SegundaOportunidad.Domain.Entities
 {
     public class Modelo
     {
-        
-       public int Modelo_ID { get; set; }
+
+        public int Modelo_ID { get; set; }
         public string Nombre { get; set; }
+        public int Marca_ID { get; set; }
+
+        public virtual ICollection<Marca> Marcas { get; set; }
 
         public Modelo()
         {
-
+            Marcas = new HashSet<Marca>();
         }
     }
 }
