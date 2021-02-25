673d2045cacd5ee0ac5736d9d243ec70eee56b78
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-25 00:01:59 -0400

    Agrega la clase Inventario y modifica la clase Almacen, remueve la propiedad Producto_ID

--- SegundaOportunidad.Domain/Entities/Almacen.cs
+++ SegundaOportunidad.Domain/Entities/Almacen.cs
@@ -10,13 +10,10 @@ namespace SegundaOportunidad.Domain.Entities
         public string Nombre { get; set; }
 
         public string Direccion { get; set; }
-        public int Producto_ID { get; set; }
-
-        public virtual ICollection<Producto> Productos { get; set; }
 
         public Almacen()
         {
-            Productos = new HashSet<Producto>();
+      
         }
     }
 }
---
+++ SegundaOportunidad.Domain/Entities/Inventario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundaOportunidad.Domain.Entities
+{
+    public class Inventario
+    {
+
+        public int Inventario_ID { get; set; }
+        public int Almacen_ID { get; set; }
+        public int Producto_ID { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Cantidad { get; set; }
+
+        public virtual ICollection<Almacen> Almacenes { get; set; }
+        public virtual ICollection<Producto> Productos { get; set; }
+        public Inventario()
+        {
+            Almacenes = new HashSet<Almacen>();
+            Productos = new HashSet<Producto>();
+        }
+    }
+}
