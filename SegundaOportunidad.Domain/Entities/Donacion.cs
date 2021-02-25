b8eef54987a51187b4e909ddb6897b56073b7987
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-25 00:07:54 -0400

    Agrega la clase Donacion al domain

---
+++ SegundaOportunidad.Domain/Entities/Donacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundaOportunidad.Domain.Entities
+{
+    public class Donacion
+    {
+        public int Donacion_ID { get; set; }
+        public int Persona_ID { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Producto_ID { get; set; }
+        public int Cantidad { get; set; }
+
+        public virtual ICollection<Persona> Personas { get; set; }
+        public virtual ICollection<Producto> Productos { get; set; }
+        public Donacion()
+        {
+            Personas = new HashSet<Persona>();
+            Productos = new HashSet<Producto>();
+        }
+    }
+}
