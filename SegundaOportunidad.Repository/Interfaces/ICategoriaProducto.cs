b30ff7c0183cde280485dcedb9ec5952b5ecc1ae
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-25 02:20:56 -0400

    Agrega la interfaz de categoria en la capa de Repositorio & crea carpeta Repositories

---
+++ SegundaOportunidad.Repository/Interfaces/ICategoriaProducto.cs
@@ -0,0 +1,10 @@
+﻿using SegundaOportunidad.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace SegundaOportunidad.Repository.Interfaces
+{
+    public interface ICategoriaProducto
+    {
+        Task <IEnumerable<CategoriaProducto>>GetAllCategories();
+    }
+}
