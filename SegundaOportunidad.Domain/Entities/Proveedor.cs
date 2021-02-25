e3c3f4117f416a9edf43fc02a2329d572b4fd411
Reinis <26393752+ReinisEspinal@users.noreply.github.com>
2021-02-24 22:12:03 -0400

    Agrega la clase Proveedor

---
+++ SegundaOportunidad.Domain/Entities/Proveedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundaOportunidad.Domain.Entities
+{
+    public class Proveedor
+    {
+        public int Proveedor_ID { get; set; }
+
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+
+        public Proveedor()
+        {
+
+        }
+    }
+}
